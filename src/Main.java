996dd5b4b406c7bce09e9eec5af133e33b785819
Sanne Delin <delinsanne@gmail.com>
2023-09-26 13:46:23 +0200

    gjort en user class

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,19 @@
 public class Main {
     public static void main(String[] args) {
 
-        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// bibblan -> get from version control -> klistra in url-länk från github.
